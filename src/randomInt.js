3a597f40b0e774c1ef0178747d794f1ae495ccb9
Sergey Klevakin <justerest@yandex.ru>
2018-01-14 01:22:20 +0500

    docs from randomInt

--- src/randomInt.js
+++ src/randomInt.js
@@ -1,3 +1,10 @@
+/**
+ * Returns random number from min to max.
+ * 
+ * @param  {number} min
+ * @param  {number} max
+ * @return {number}
+ */
 module.exports = function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
